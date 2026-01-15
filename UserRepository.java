5533be6b6604d352287ac3dd6fe3102a5e416333
Aaarya2117 <aaarya.gr@gmail.com>
2026-01-16 03:10:16 +0530

    Create UserRepository.java file

---
+++ UserRepository.java
@@ -0,0 +1,13 @@
+package com.tastopia.app.repository;
+
+import com.tastopia.app.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+    User findByUsername(String username);
+    User findByEmail(String email);
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+}
